c60b663438f5ff13c8095eead76ddec56fcd5e22
Steven <otienosteven1233@gmail.com>
2026-05-15 01:06:16 +0300

    fixed production settings

--- laundry_backend/settings.py
+++ laundry_backend/settings.py
@@ -13,15 +13,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xns&xq^j5-y8b=$2p!e+u*h0s0gqqyi7gxxyeo2*vr3)lb4iy8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    "https://drymebackend-2.onrender.com/",
+    "drymebackend-2.onrender.com",
     "localhost",
     "127.0.0.1",
-
-]  # allow all hosts during development 
+]
 
 # Application definition
 INSTALLED_APPS = [
@@ -31,13 +29,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
-    'dryMe',
     'corsheaders',
+
+    'dryMe',
 ]
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,12 +50,16 @@ MIDDLEWARE = [
 
 # CORS settings
 CORS_ALLOWED_ORIGINS = [
-    "https://dry-me-frontend.vercel.app/",
+    "https://dry-me-frontend.vercel.app",
     "http://localhost:5173",
-      
 ]
-# CORS_ALLOW_ALL_ORIGINS = True  # convenient for dev, disable in production
+
 CORS_ALLOW_ALL_ORIGINS = False
+
+# CSRF trusted origins
+CSRF_TRUSTED_ORIGINS = [
+    "https://dry-me-frontend.vercel.app",
+]
 
 ROOT_URLCONF = 'laundry_backend.urls'
 
@@ -66,7 +71,8 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',  # needed for absolute URLs
+                'django.template.context_processors.request',
+
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -83,31 +89,32 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-#here mpesa
-# from decouple import config
-
-# MPESA_CONSUMER_KEY = config("MPESA_CONSUMER_KEY")
-# MPESA_CONSUMER_SECRET = config("MPESA_CONSUMER_SECRET")
-# MPESA_SHORTCODE = config("MPESA_SHORTCODE")
-# MPESA_PASSKEY = config("MPESA_PASSKEY")
-# CALLBACK_URL = config("CALLBACK_URL")
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'
+    },
 ]
 
 # Custom user model
-
 AUTH_USER_MODEL = 'dryMe.User'
 
+# DRF + JWT
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
@@ -117,22 +124,22 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
-# Media files (for image uploads)
+
+# Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Static files
 STATIC_URL = '/static/'
 
-# Only include STATICFILES_DIRS if you actually have a global static folder
-# STATICFILES_DIRS = [BASE_DIR / "static"]
-
-# Absolute URL settings (important for DRF serializers)
+# File storage
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 
 # Internationalization
 LANGUAGE_CODE = 'en-us'
+
 TIME_ZONE = 'UTC'
+
 USE_I18N = True
 USE_TZ = True
 
